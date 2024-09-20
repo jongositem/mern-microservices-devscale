5e0244b10e0e30a68e729a68d5e3d0787e0cb002
leoadjie <leoadjie23@gmail.com>
2024-09-20 14:08:47 +0700

    update files and abstractionn

---
+++ users/src/infrastructure/models/user.schema.ts
@@ -0,0 +1,7 @@
+import {model, Schema } from "mongoose";
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String
+})
+export const User = model("User",userSchema);
